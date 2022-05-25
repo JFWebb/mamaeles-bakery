3f726482a1da9e978d47f26167e192ebcada3727
Julie Webb <julief.webb@gmail.com>
2022-05-24 22:09:51 -0400

    create & post routes up and running

---
+++ models/productSeed.js
@@ -0,0 +1,20 @@
+module.exports = [{
+    name: 'Beans',
+    description: 'A small pile of beans. Buy more beans for a big pile of beans.',
+    image: 'https://imgur.com/LEHS8h3.png',
+    price: 5,
+    qty: 99
+  }, {
+    name: 'Bones',
+    description: "It's just a bag of bones.",
+    image: 'https://imgur.com/dalOqwk.png',
+    price: 25,
+    qty: 0
+  }, {
+    name: 'Bins',
+    description: 'A stack of colorful bins for your beans and bones.',
+    image: 'https://imgur.com/ptWDPO1.png',
+    price: 7000,
+    qty: 1
+  }
+];
--- server.js
+++ server.js
@@ -2,18 +2,21 @@
 // DEPENDCIES & DATA VARIABLES
 // =================================================
 // -- packages
-require("dotenv").config();
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const methodOverride = require("method-override");
-const { default: mongoose } = require("mongoose");
+const mongoose = require('mongoose');
+// const { create } = require("./models/products.js");
 const PORT = process.env.PORT || 3000;
+const Product = require("./models/product.js");
 
 // -- databases
 mongoose.connect(process.env.DATABASE_URL, {
-    useNewURLParser: true,
-    useUnifiedTopology: true,
+	useNewUrlParser: true,
+	useUnifiedTopology: true
 });
+
 
 // ------ database connections error/success
 const db = mongoose.connection
@@ -26,7 +29,7 @@ db.on('disconnected', () => console.log('mongo disconnected'));
 // MOUNT MIDDLEWARE
 // =================================================
 // -- encode input data to req.body
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // -- method-override for deletes & puts
 app.use(methodOverride("_method"));
@@ -37,11 +40,44 @@ app.use(express.static("public"));
 // =================================================
 // MOUNT ROUTES
 // =================================================
+//Seed
+const productSeed = require("./models/productSeed.js");
+app.get("/shop/seed", (req, res) => {
+    Product.deleteMany({}, (error, allProducts) => {});
+    Product.create(productSeed, (error, allProducts) => {
+        res.redirect("/shop");
+    })
+})
+    
+
 // Index
+app.get("/shop", (req, res) => {
+    Product.find({}, (error, allProducts) => {
+        res.render("index.ejs", {
+            products: allProducts,
+        });
+    });
+});
+
 // New
+app.get("/shop/new", (req, res) => {
+    res.render("new.ejs");
+});
 // Destroy
 // Update
 // Create
+app.post('/shop', (req, res) => {
+    req.body.price = Number(req.body.price);
+    req.body.qty = Number (req.body.qty)
+    console.log("req.body :")
+    console.log(req.body)
+    Product.create(req.body, (error, createdProduct) => {
+        console.log("created product:")
+        console.log(createdProduct);
+        res.redirect("/shop");
+        console.log(error)
+    });   
+});
 // Edit
 // Show
 
