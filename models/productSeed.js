eeb18d1228a94109619c2e3ebcb2524f137a7d5f
Julie Webb <julief.webb@gmail.com>
2022-05-27 11:27:39 -0400

    add HTML tab icon

--- models/productSeed.js
+++ models/productSeed.js
@@ -2,43 +2,43 @@ module.exports = [{
     name: 'Blintz',
     description: 'A rolled pancake filled with cheese and jam, then fried.',
     image: 'https://i.imgur.com/7QjGIgl.jpg',
-    price: 3,
+    price: 3.00,
     qty: 99
   }, {
     name: 'Babka',
     description: "Sweet bread braided with stripes of rich chocolate.",
     image: 'https://i.imgur.com/nVboUmR.jpg',
-    price: 25,
+    price: 10.00,
     qty: 0
   }, {
     name: 'Sufganiyot',
     description: 'Donut filled with jelly and topped with powdered sugar.',
     image: 'https://i.imgur.com/f21znqy.png',
-    price: 3,
+    price: 4.00,
     qty: 50
   }, {
     name: 'Challah',
     description: 'Braided load of bread.',
     image: 'https://i.imgur.com/NMVSBEx.jpg',
-    price: 5,
+    price: 5.00,
     qty: 30
   },{
     name: 'Hamentashen',
     description: 'Triangular filled-pocket pastry.',
     image: 'https://i.imgur.com/BnsQzbo.jpg',
-    price: 1,
+    price: 1.00,
     qty: 100
   },{
     name: 'Halvah',
     description: 'Confectionary made of seaseme seed paste and sweeted with honey, mixed with chocolate or nuts.',
     image: 'https://i.imgur.com/kDaqWGi.jpg',
-    price: 3,
+    price: 2.00,
     qty: 20
   },{
     name: 'Rugelach',
     description: 'The word “Rugelach” is Yiddish that means “little twists” and are cookies that can be filled with a variety of ingredients.',
     image: 'https://i.imgur.com/PBvwoDJ.jpg',
-    price: 1,
+    price: 1.00,
     qty: 20
   },{
     name: 'Mandelbrot',
